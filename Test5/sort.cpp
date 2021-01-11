f347ee881d2b5c96aaf08519ebede9b93127718b
unknown <18721510380@163.com>
2021-01-11 23:39:13 +0800

    21.01.11

---
+++ Test5/sort.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+template<class T>
+void swap(T& a, T& b) {
+    T temp;
+    temp = a;
+    a = b;
+    b = temp;
+
+}
+template <class T>
+void bubble(T a[],int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n-i-1; j++) {
+            if (a[j] > a[j+1]) swap<T>(a[j],a[j+1]);
+        }
+    }
+
+}
