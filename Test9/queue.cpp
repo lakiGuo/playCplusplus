c842154ccce0206348584f839aaad11677331b2e
unknown <18721510380@163.com>
2021-01-21 23:15:06 +0800

    add abstract class

---
+++ Test9/queue.cpp
@@ -0,0 +1,12 @@
+template<class T>
+class queque
+{
+	public:
+		virtual ~queque(){}
+		virtual bool empty() const=0;
+		virtual int size() const=0;
+		virtual T& front()=0;
+		virtual T& back()=0;
+		virtual void pop()=0;
+		virtual void push(const T& theElement)=0;
+}
