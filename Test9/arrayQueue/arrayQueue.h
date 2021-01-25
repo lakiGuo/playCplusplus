9d557ecec5a5712aa81005bfb21e01f15a94c794
unknown <18721510380@163.com>
2021-01-25 23:49:16 +0800

    21.1.25

--- Test9/arrayQueue/arrayQueue.h
+++ Test9/arrayQueue/arrayQueue.h
@@ -1,4 +1,4 @@
-#pragma once
+
 #ifndef ARRAYQUEUE_H
 #define ARRAYQUEUE_H
 
@@ -18,13 +18,16 @@ class arrayQueue :public queue<T>
 		T& front() { if (theFront == theBack) throw "queue is empty";
 					 return queue[(theFront + 1) % arrayLength];
 		}
-		T& back() { if (theFront == theBack) throw "queue is empty";
-					return queue[theBack];
+		T& back() {
+			if (theFront == theBack) throw "queue is empty";
+			return queue[theBack];
+		}
 	    void pop() {
 			if (theFront == theBack) throw "queue is empty";
-		
+			theFront = (theFront + 1) % arrayLength;
+			queue[theFront].~T();
 		}
-
+		void push(const T& theElement);
 	private:
 		int theFront;
 		int theBack;
@@ -43,5 +46,24 @@ arrayQueue<T>::arrayQueue(int initialCapacity){
 	theFront = 0;
 	theBack = 0;
 }
-
+template<class T>
+void arrayQueue<T>::push(const T& theElement){
+	if ((theBack + 1) % arrayLength == theFront) {
+		T* newQueue = new T[2 * arrayLength];
+		int start = (theFront + 1) % arrayLength;
+		if (start < 2)
+			copy(queue + start, queue + start + arrayLength - 1, newQueue);
+		else
+		{
+			copy(queue + start, queue + arrayLength, newQueue);
+			copy(queue, queue + theBack + 1, newQueue + arrayLength - start);
+		}
+		theFront = 2 * arrayLength - 1;
+		theBack = arrayLength - 2;
+		arrayLength *= 2;
+		queue = newQueue;
+	}
+	theBack = (theBack + 1) % arrayLength;
+	queue[theBack] = theElement;
+}
 #endif
